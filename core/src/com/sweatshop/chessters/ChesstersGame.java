06965304303875ae7f8c7ab1e33a4bc9d51f8ef1
Crashaholic <33598082+Crashaholic@users.noreply.github.com>
2024-04-15 21:51:34 +0800

    code changes

--- core/src/com/sweatshop/chessters/ChesstersGame.java
+++ core/src/com/sweatshop/chessters/ChesstersGame.java
@@ -1,30 +1,67 @@
 package com.sweatshop.chessters;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.math.Rectangle;
 
 public class ChesstersGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	private OrthographicCamera camera;
+	private Rectangle bucket;
+	private Texture bucketTexture;
 	
 	@Override
-	public void create () {
+	public void create()
+	{
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 600, 600);
+
+		bucket = new Rectangle(368, 20, 64, 64);
+		bucketTexture = new Texture("1007PART2TUT.png");
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+	public void render()
+	{
+		if (Gdx.input.isTouched())
+		{
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			bucket.x = touchPos.x - 32;
+		}
+
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+			bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+			bucket.x += 200 * Gdx.graphics.getDeltaTime();
+
+		if (bucket.x < 0)
+			bucket.x = 0;
+		if (bucket.x > 800 - 64)
+			bucket.x = 800 - 64;
+
+		ScreenUtils.clear(1, 1, 1, 1);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
+		batch.draw(bucketTexture, bucket.x, bucket.y);
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
 	@Override
-	public void dispose () {
+	public void dispose()
+	{
 		batch.dispose();
 		img.dispose();
 	}
